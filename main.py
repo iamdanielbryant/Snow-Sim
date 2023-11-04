ec03ff31b8b51bb8e0f563b2b43b7fbba0290fbe
Daniel Bryant <danbryant1426@gmail.com>
2023-11-04 14:28:54 -0700

    Re-factored snow object into class

--- main.py
+++ main.py
@@ -1,34 +1,45 @@
 import pygame
+from screeninfo import get_monitors
+
 import snow as snowlib
 
-width = 640
-height = 480
-
+#pygame setup
 pygame.init()
-screen = pygame.display.set_mode((width,height))
-surface = pygame.Surface((width,height), pygame.SRCALPHA)
-
 clock = pygame.time.Clock()
 running = True
 
+#screen setup
+monitorWidth = get_monitors()[0].width
+monitorHeight = get_monitors()[0].height
+gameWidth = monitorWidth / 1.5
+gameHeight = monitorHeight / 1.5
+
+pygame.display.set_caption("Snow Simulator")
+screen = pygame.display.set_mode((gameWidth,gameHeight))
+surface = pygame.Surface((gameWidth,gameHeight), pygame.SRCALPHA)
+
+#game objects
 snow = snowlib.Snow(surface)
 
+#gameloop
 while running:
     surface.fill("black")
-
     snow.updateFlakes()
 
+    #close window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    
 
     #render game here
     snow.renderSnow() 
 
+    #pygame stuff
     screen.blit(surface,(0,0))
     pygame.display.flip()
-
     clock.tick(60)
+    
 
 pygame.quit()
 
--- snow.py
+++ snow.py
@@ -1,43 +1,50 @@
 import pygame
 import random
 
+class SnowFlake:
+
+    def __init__(self, screenWidth,screenHeight):
+        randW = random.uniform(0,screenWidth)
+        randH = random.uniform(-screenHeight,screenHeight)
+
+        pos = pygame.Vector2(randW,randH)
+
+        self.position = pos
+        self.size = random.uniform(1,3)
+        self.opacity = random.uniform(0,255)
+        self.speed = 1
+        self.xFactor = random.uniform(-1,1)
+        self.yFactor = random.uniform(-1,1)
+
+
 class Snow:
 
     def __init__(self,surface):
+
         self.maxFlakes = 100
         self.surface = surface
         self.screenWidth = surface.get_width()
         self.screenHeight = surface.get_height()
+
         self.flakes = []
         self.gravity = 1
 
         for i in range(self.maxFlakes):
-            randW = random.uniform(0,self.screenWidth)
-            randH = random.uniform(-self.screenHeight,self.screenHeight)
-
-            pos = pygame.Vector2(randW,randH)
-            snowEntity = {
-                "position": pos, 
-                "size": random.uniform(1,2),
-                "opacity": random.uniform(0,255),
-                "speed": 1,
-                "xFactor":random.uniform(-1,1), 
-                "yFactor":random.uniform(-1,1)
-            }
-            self.flakes.append(snowEntity)
+            flake = SnowFlake(self.screenWidth,self.screenHeight)
+            self.flakes.append(flake)
 
     def updateFlakes(self):
         for s in self.flakes:
-            s["position"].x += s["speed"] * s["xFactor"]
-            s["position"].y += s["speed"] * s["yFactor"] + self.gravity
+            s.position.x += s.speed * s.xFactor
+            s.position.y += s.speed * s.yFactor + self.gravity
 
-            if (s["position"].x > self.screenWidth or s['position'].y > self.screenHeight):
-                s["position"].x = random.uniform(0,self.screenWidth)
-                s["position"].y = random.uniform(0,-self.screenHeight)
+            if (s.position.x > self.screenWidth or s.position.y > self.screenHeight): 
+                s.position.x = random.uniform(0,self.screenWidth)
+                s.position.y = random.uniform(0,-self.screenHeight)
 
     def renderSnow(self):
         for s in self.flakes:
-            pygame.draw.circle(self.surface, (255,255,255,s['opacity']),s["position"],s["size"])
+            pygame.draw.circle(self.surface, (255,255,255,s.opacity),s.position,s.size)
         
     
 
